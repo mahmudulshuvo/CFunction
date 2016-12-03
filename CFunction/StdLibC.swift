f6dfd0e40fd759479ed9a9fb49cacfc4830d6776
mahmudulshuvo <shuvo0201@gmail.com>
2016-12-03 18:47:16 +0600

    Added functions to StdLib class

--- CFunction/StdLibC.swift
+++ CFunction/StdLibC.swift
@@ -15,7 +15,9 @@ class StdLibC  {
     init()  {
         swiftTest()
         simpleFunctions()
-      //  strtodC()
+        wcstombFunc()
+        complexFunctions()
+       // strtodC()
     }
     
     func  swiftTest() {
@@ -95,25 +97,118 @@ class StdLibC  {
         result = getenv(&getString.0)
         print("result of getenv function: \(result)")
         
-       // public func strtod(_: UnsafePointer<Int8>!, _: UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>!) -> Double
+        var valI:Int8 = 123
+        let add = ptrFromAddress(p: &valI)
+        result = a64l(add)
+        print("result of a641: \(result)")
+        
+        result = drand48()
+        print("result of drand48: \(result)")
+        
+    }
+
+    func wcstombFunc()  {
+        
+        var val:Int8 = 123
+        let p = ptrFromAddress(p: &val)
+        
+        var valWchar:wchar_t = 1233432
+        let q = ptrFromAddress(p: &valWchar)
+        
+        result = wcstombs(p, q, 5)
+        print("result of wcstombs: \(result)")
+        
+        result = wctomb(p,valWchar)
+        print("result of wctomb: \(result)")
         
     }
     
-    
-    func strtodC() {
+
+    func complexFunctions() {
         
+        var val32:Int32 = 123
+        let p = ptrFromAddress(p: &val32)
+        
+        result = ecvt(3434.0324, val32, p, p)
+        print("result of ecvt: \(result)")
+        
+        var valU16:UInt16 = 4342
+        let pValU16 = ptrFromAddress(p: &valU16)
+        
+        result = erand48(pValU16)
+        print("result of erand48: \(result)")
+        
+        result = fcvt(100, val32, p, p)
+        print("result of fcvt: \(result)")
+        
+        var val8:Int8 = 12
+        let pVal8 = ptrFromAddress(p: &val8)
+        
+        result = gcvt(123.99, val32, pVal8)
+        print("result of gcvt: \(result)")
+        
+        result = grantpt(3423235)
+        print("result of grantpt: \(result)")
+        
+        let valU32:UInt32 = 34234
+        result = initstate(valU32, pVal8, 3)
+        print("result of inistate: \(result)")
+        
+        result = l64a(6)
+        print("result of 164a: \(result)")
+        
+        result = lcong48(pValU16)
+        print("result of lcong48: \(result)")
+        
+        result = posix_openpt(4323545)
+        print("result of posix_openpt: \(result)")
+        
+        result = ptsname(45934859)
+        print("result of ptsname: \(result)")
+        
+        result = putenv(pVal8)
+        print("result of putenv: \(result)")
+        
+        result = realpath(pVal8, pVal8)
+        print("result of realpath: \(result)")
+        
+        result = seed48(pValU16)
+        print("result of seed48: \(result)")
+        
+        result = setenv(pVal8, pVal8, 453545)
+        print("result of setenv: \(result)")
+        
+        result = setstate(pVal8)
+        print("result of setstate: \(result)")
+        
+        result = unsetenv(pVal8)
+        print("result of unsetenv: \(result)")
+        
+    }
+
+    
+    
+    func ptrFromAddress<T>(p:UnsafeMutablePointer<T>) -> UnsafeMutablePointer<T>
+    {
+        return p
+    }
+    
+    
+    
+//    func strtodC() {
+    
 //        var xStr:Int8 = 123
 //        let yStr:Int8 = 5
-        
+//        
 //        let x : UnsafePointer<Int8>? = nil
 //        let y : UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>? = nil
 //        
 //        result = strtod(x, y)
 //        print("result of strtod: \(result)")
-        
+//        
 //        var nInt:Int8 = 123
 //        let p = ptrFromAddress(p: &nInt)
-//
+//        
 //        
 //        var nestedInt:Int8 = 5
 //        var r = ptrFromAddress(p: &nestedInt)
@@ -122,29 +217,22 @@ class StdLibC  {
 //        result = strtod(p,s)
         
 //        withUnsafeMutablePointer(&nInt) { (var w) in
-// 
+//        
 //        }
 //        
 //        withUnsafeMutablePointer(&myInt) { (var p) in
 //            withUnsafeMutablePointer(&p) { (var pp) in
-//
+//        
 //            }
 //        }
 //        
 //        // sting copy method with safe memory allocation
 //        withUnsafeMutablePointer(to: &string1) { str1Ptr in
-//                withUnsafePointer(to: &string2) { str2Ptr in
-//                        _ = strcpy(str1Ptr, str2Ptr)
-//                    }
-//                }
-    }
-    
-    func ptrFromAddress<T>(p:UnsafeMutablePointer<T>) -> UnsafeMutablePointer<T>
-    {
-        return p
-    }
-    
-
+//            withUnsafePointer(to: &string2) { str2Ptr in
+//                    _ = strcpy(str1Ptr, str2Ptr)
+//            }
+//        }
+//    }
 
     
 }
